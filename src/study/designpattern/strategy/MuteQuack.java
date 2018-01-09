7e81160a3c8f929822e054844097e22d7c963377
Kyujin Kang <digiry@gmail.com>
2018-01-10 03:23:58 +0900

    Add strategy pattern

---
+++ src/study/designpattern/strategy/MuteQuack.java
@@ -0,0 +1,7 @@
+package study.designpattern.strategy;
+
+public class MuteQuack implements QuackBehavior {
+    public void quack() {
+        System.out.println("<< 조용~ >>");
+    }
+}
---
+++ src/study/designpattern/strategy/Quack.java
@@ -0,0 +1,7 @@
+package study.designpattern.strategy;
+
+public class Quack implements QuackBehavior {
+    public void quack() {
+        System.out.println("꽥");
+    }
+}
